e7c671d98ffe98516194afb4ffabf159a659f3a0
Michael <mfw6mw@virginia.edu>
2021-03-03 22:32:59 -0500

    commit tree 3656a47e3efce3f70e54957eecd5c9d1581d6346

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        self.word=word
+    end
+    def match(words)
+        match=[]
+        words.select do |word|
+            if word.split("").sort==self.word.split("").sort 
+                match << word
+            end    
+        end
+        match
+    end
+end 
+
